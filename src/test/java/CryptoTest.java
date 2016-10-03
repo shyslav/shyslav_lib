85b5d39916a44d057646ec839342ac78cbebcc3c
shyslav <shyshkin.vladyslav@gmail.com>
2016-10-03 23:52:36 +0300

    updated crypt method

--- src/test/java/CryptoTest.java
+++ src/test/java/CryptoTest.java
@@ -3,10 +3,15 @@ import org.junit.Test;
 import sun.misc.BASE64Encoder;
 
 import javax.crypto.*;
+import javax.crypto.spec.DESedeKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 /**
@@ -108,5 +113,72 @@ public class CryptoTest {
         } catch (InvalidAlgorithmParameterException invalidParam) {
             System.out.println(" Invalid Parameter " + invalidParam);
         }
+    }
+
+    @Test
+    public void cryptEx2() throws Exception {
+
+        String text = "kyle boon";
+
+        byte[] codedtext = encrypt(text);
+        String decodedtext = decrypt(codedtext);
+
+        System.out.println(codedtext); // this is a byte array, you'll just see a reference to an array
+        System.out.println(decodedtext); // This correctly shows "kyle boon"
+    }
+
+    public byte[] encrypt(String message) throws Exception {
+        final MessageDigest md = MessageDigest.getInstance("md5");
+        final byte[] digestOfPassword = md.digest("HG58YZ3CR9"
+                .getBytes("utf-8"));
+        final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+        for (int j = 0, k = 16; j < 8; ) {
+            keyBytes[k++] = keyBytes[j++];
+        }
+        String myEncryptionKey = LazyMD5.md5("ThisIsSpartaThisIsSparta");
+        byte[] arrayBytes = myEncryptionKey.getBytes();
+        KeySpec ks = new DESedeKeySpec(arrayBytes);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("DESede");
+        SecretKey key = skf.generateSecret(ks);
+
+
+        //final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+        final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+        final Cipher cipher = Cipher.getInstance("DESede/CFB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+        final byte[] plainTextBytes = message.getBytes("utf-8");
+        final byte[] cipherText = cipher.doFinal(plainTextBytes);
+        // final String encodedCipherText = new sun.misc.BASE64Encoder()
+        // .encode(cipherText);
+        System.out.println(new BASE64Encoder().encode(cipherText));
+
+        return cipherText;
+    }
+
+    public String decrypt(byte[] message) throws Exception {
+        final MessageDigest md = MessageDigest.getInstance("md5");
+        final byte[] digestOfPassword = md.digest("HG58YZ3CR9"
+                .getBytes("utf-8"));
+        final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+        for (int j = 0, k = 16; j < 8; ) {
+            keyBytes[k++] = keyBytes[j++];
+        }
+        String myEncryptionKey = LazyMD5.md5("ThisIsSpartaThisIsSparta");
+        byte[] arrayBytes = myEncryptionKey.getBytes();
+        KeySpec ks = new DESedeKeySpec(arrayBytes);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("DESede");
+        SecretKey key = skf.generateSecret(ks);
+
+//        final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+        final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+        final Cipher decipher = Cipher.getInstance("DESede/CFB/PKCS5Padding");
+        decipher.init(Cipher.DECRYPT_MODE, key, iv);
+
+        // final byte[] encData = new
+        // sun.misc.BASE64Decoder().decodeBuffer(message);
+        final byte[] plainText = decipher.doFinal(message);
+
+        return new String(plainText, "UTF-8");
     }
 }
