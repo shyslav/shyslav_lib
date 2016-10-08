56f54652fe493221766734479ad7e52ab558869f
shyslav <shyshkin.vladyslav@gmail.com>
2016-10-08 18:21:55 +0300

    added to database connection encode and decode action

--- src/main/java/database/configuration/DatabaseConnection.java
+++ src/main/java/database/configuration/DatabaseConnection.java
@@ -1,9 +1,11 @@
 package database.configuration;
 
+import crypto.LazyCrypto;
 import org.apache.log4j.Logger;
+import sun.misc.BASE64Decoder;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.Properties;
 
@@ -17,31 +19,33 @@ public class DatabaseConnection {
     /**
      * Open mysql connection and create statement
      */
-    public void openConnection()
-    {
+    public void openConnection() {
         ClassLoader classLoader = getClass().getClassLoader();
         Properties props = new Properties();
         try (InputStream in = classLoader.getResourceAsStream("database/database.properties")) {
-            props.load(in);
+            String inputStreamText = getStringFromInputStream(in);
+            byte[] bytes = new BASE64Decoder().decodeBuffer(inputStreamText);
+            String decryptText = LazyCrypto.decryptText(bytes);
+            InputStream stream = new ByteArrayInputStream(decryptText.getBytes(StandardCharsets.UTF_8));
+            props.load(stream);
         } catch (IOException ex) {
             log.error("cannot be read properties file" + ex);
         }
-        try{
+        try {
             Class.forName(props.getProperty("jdbc.drivers"));
-            con = DriverManager.getConnection(props.getProperty("jdbc.url"),props.getProperty("jdbc.username"),props.getProperty("jdbc.password"));
+            con = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.username"), props.getProperty("jdbc.password"));
             st = con.createStatement();
-        }
-        catch(ClassNotFoundException | SQLException ex)
-        {
-            log.error("open connection error"+ex);
+        } catch (ClassNotFoundException | SQLException ex) {
+            log.error("open connection error" + ex);
         }
     }
 
     /**
      * Get connection without statement
+     *
      * @return connection
      */
-    public Connection getConnection(){
+    public Connection getConnection() {
         ClassLoader classLoader = getClass().getClassLoader();
         Properties props = new Properties();
         try (InputStream in = classLoader.getResourceAsStream("database/database.properties")) {
@@ -51,19 +55,46 @@ public class DatabaseConnection {
         }
         try {
             Class.forName(props.getProperty("jdbc.drivers"));
-            con = DriverManager.getConnection(props.getProperty("jdbc.url"),props.getProperty("jdbc.username"),props.getProperty("jdbc.password"));
+            con = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.username"), props.getProperty("jdbc.password"));
         } catch (ClassNotFoundException | SQLException ex) {
-            log.error("open connection error"+ex);
+            log.error("open connection error" + ex);
         }
         return con;
     }
 
-    public void closeConnection()
-    {
+    public void closeConnection() {
         try {
             con.close();
         } catch (SQLException ex) {
-            log.error("close database connection error"+ex);
+            log.error("close database connection error" + ex);
         }
+    }
+
+    // convert InputStream to String
+    private static String getStringFromInputStream(InputStream is) {
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
     }
 }
--- src/test/java/LazyCryptoTest.java
+++ src/test/java/LazyCryptoTest.java
@@ -1,7 +1,10 @@
 import crypto.LazyCrypto;
+import database.configuration.DatabaseConnection;
 import org.junit.Test;
+import sun.misc.BASE64Encoder;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by shyslav on 10/4/16.
@@ -9,11 +12,21 @@ import static org.junit.Assert.*;
 public class LazyCryptoTest {
     @Test
     public void lazyCryptTest() {
-        String codeString = "Hello vasya pupkin";
+        String codeString = "jdbc.drivers=com.mysql.jdbc.Driver\n" +
+                "jdbc.url=jdbc:mysql://127.0.0.1:3306/protect_information?useUnicode=true&characterEncoding=utf-8\n" +
+                "jdbc.username=root\n" +
+                "jdbc.password=12345";
         byte[] encode = LazyCrypto.cryptString(codeString);
+        String decodedString = new BASE64Encoder().encode(encode);
         String test = LazyCrypto.decryptText(encode);
         assertNotNull(encode);
         assertNotNull(test);
-        assertEquals(test,codeString);
+        assertEquals(test, codeString);
+    }
+
+    @Test
+    public void lazyDatabaseCryptoConnectionTest() {
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        databaseConnection.openConnection();
     }
 }
